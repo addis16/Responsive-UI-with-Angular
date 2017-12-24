c5b9357d51de9b8f20a5eb7178e77e1e3b0a7dcd
Jacob Spencer Addis <j.addis@setonhill.edu>
2017-12-24 09:04:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,14 @@
             }
             $scope.adjust = function() {
                 if ($scope.width <= 1322) {
-                    $scope.columnStyle = { boxShadow: 'none', borderBottom: '3px solid black'};
+                    $scope.columnStyle = { boxShadow: 'none', borderBottom: '3px solid black'}; // just an example, can be anything.
+                     // any code here will run when the window width is <= 1322 pixels.
                 }
                 else if($scope.width > 1322) {
-                    $scope.columnStyle = { boxShadow: '0px 3px 3px black', borderBottom: 'none'};                
+                    $scope.columnStyle = { boxShadow: '0px 3px 3px black', borderBottom: 'none'}; // another example.
+                    // any code here will run when the window width is greater than 1322 pixels.
+                    // I think this is better than media queries, because you can execute scripts instead of just changing css.
+                    // If you're using Angular in your project already of course.    
                 }
             } 
                 $scope.updateHeight();
